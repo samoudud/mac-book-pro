384bb8f8dcd29d9cf716e48b3075804e89590021
samoudud <moududpro@gmail.com>
2021-08-21 02:40:47 +0600

    bonus part added

--- js/app.js
+++ js/app.js
@@ -3,25 +3,35 @@ function updateExtra(idName, price) {
     const selectExtra = document.getElementById(idName);
     selectExtra.innerText = price;
 }
+
+// function to get price
 function getPrice(idName) {
     const priceText = document.getElementById(idName);
     const price = parseInt(priceText.innerText);
     return price;
 }
+
 // update total price function
 function updateTotalPrice() {
+    // getting prices
     const bestPrice = getPrice('best-price');
     const extraMemoryCost = getPrice('memory-cost');
     const extraStorageCost = getPrice('storage-cost');
     const deliveryCharge = getPrice('delivery-cost');
-    const totalPrice = bestPrice + extraMemoryCost + extraStorageCost + deliveryCharge;
 
     const totalPriceAmount = document.getElementById('total-price');
+    const totalAmount = document.getElementById('total');
+
+    // calculate total price
+    const totalPrice = bestPrice + extraMemoryCost + extraStorageCost + deliveryCharge;
+
+    // update total price and Total
     totalPriceAmount.innerText = totalPrice;
+    totalAmount.innerText = totalPrice;
 
 }
 
-//                    Memory buttons events                             ///
+//******************Memory buttons events******************//
 // 8GB Memory button event
 document.getElementById('memory-8gb').addEventListener('click', function () {
     updateExtra('memory-cost', 0);
@@ -35,7 +45,7 @@ document.getElementById('memory-16gb').addEventListener('click', function () {
 });
 
 
-//                    storage buttons events                             ///
+//******************storage buttons events******************//
 // 256GB storage button event
 document.getElementById('storage-256gb').addEventListener('click', function () {
     updateExtra('storage-cost', 0);
@@ -56,7 +66,7 @@ document.getElementById('storage-1tb').addEventListener('click', function () {
 
 
 
-//                    delivery buttons events                             ///
+//******************delivery buttons events******************//
 // Aug 25 delivery button event
 document.getElementById('delivery-regular').addEventListener('click', function () {
     updateExtra('delivery-cost', 0);
@@ -67,4 +77,23 @@ document.getElementById('delivery-regular').addEventListener('click', function (
 document.getElementById('delivery-argent').addEventListener('click', function () {
     updateExtra('delivery-cost', 20);
     updateTotalPrice();
+});
+
+
+//******************Bonus Part******************//
+// promo apply event 
+document.getElementById('apply-button').addEventListener('click', function () {
+    const promoInput = document.getElementById('promo-input');
+    const promoInputText = promoInput.value.toLowerCase();
+
+    if (promoInputText == 'stevekaku') {
+        const total = document.getElementById('total');
+        const totalPrice = getPrice('total-price');
+
+        const discountedPrice = totalPrice - (totalPrice * .2);
+
+        // update price after discount
+        total.innerText = discountedPrice;
+    }
+    promoInput.value = '';
 });
