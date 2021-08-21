a24046b349de17bbf2f499f63ba3d18ff6b4fc4e
samoudud <moududpro@gmail.com>
2021-08-21 19:07:06 +0600

    code reduced

--- js/app.js
+++ js/app.js
@@ -31,53 +31,14 @@ function updateTotalPrice() {
 
 }
 
-//******************Memory buttons events******************//
-// 8GB Memory button event
-document.getElementById('memory-8gb').addEventListener('click', function () {
-    updateExtra('memory-cost', 0);
+
+function buttonEvent(idName, price) {
+    // update extra price
+    updateExtra(idName, price);
+
+    // update total price
     updateTotalPrice();
-});
-
-// 16GB Memory button event
-document.getElementById('memory-16gb').addEventListener('click', function () {
-    updateExtra('memory-cost', 180);
-    updateTotalPrice();
-});
-
-
-//******************storage buttons events******************//
-// 256GB storage button event
-document.getElementById('storage-256gb').addEventListener('click', function () {
-    updateExtra('storage-cost', 0);
-    updateTotalPrice();
-});
-
-// 512GB storage button event
-document.getElementById('storage-512gb').addEventListener('click', function () {
-    updateExtra('storage-cost', 100);
-    updateTotalPrice();
-});
-
-// 1TB storage button event
-document.getElementById('storage-1tb').addEventListener('click', function () {
-    updateExtra('storage-cost', 180);
-    updateTotalPrice();
-});
-
-
-
-//******************delivery buttons events******************//
-// Aug 25 delivery button event
-document.getElementById('delivery-regular').addEventListener('click', function () {
-    updateExtra('delivery-cost', 0);
-    updateTotalPrice();
-});
-
-// Aug 21 delivery button event
-document.getElementById('delivery-argent').addEventListener('click', function () {
-    updateExtra('delivery-cost', 20);
-    updateTotalPrice();
-});
+}
 
 
 //******************Bonus Part******************//
